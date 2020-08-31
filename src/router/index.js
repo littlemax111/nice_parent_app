dcf167250e7b789e571a1459fd87af5fb6895464
chenyuwangzi <15558177820@163.com>
2020-08-31 10:22:22 +0800

    修改

--- src/router/index.js
+++ src/router/index.js
@@ -18,49 +18,49 @@ export default new Router({
     // 网校
     {
       path: "/homePage/online",
-      name: "homePage",
+      name: "online",
       component: () => import('@/views/homePage/online'),
     },
     // 校区
     {
       path: "/homePage/school",
-      name: "homePage",
+      name: "school",
       component: () => import('@/views/homePage/school'),
     },
     // 公开课列表
     {
       path: "/homePage/publicClass",
-      name: "homePage",
+      name: "publicClass",
       component: () => import('@/views/homePage/publicClass'),
     },
     // 公开课详情
     {
       path: "/homePage/classDetail",
-      name: "homePage",
+      name: "classDetail",
       component: () => import('@/views/homePage/classDetail'),
     },
     // 资讯列表
     {
       path: "/homePage/news",
-      name: "homePage",
+      name: "news",
       component: () => import('@/views/homePage/news'),
     },
     // 资讯详情
     {
       path: "/homePage/newsDetail",
-      name: "homePage",
+      name: "newsDetail",
       component: () => import('@/views/homePage/newsDetail'),
     },
     // 在读年级
     {
       path: "/homePage/grade",
-      name: "homePage",
+      name: "grade",
       component: () => import('@/views/homePage/grade'),
     },
     // 选择城市
     {
       path: "/homePage/city",
-      name: "homePage",
+      name: "city",
       component: () => import('@/views/homePage/city'),
     },
     // 选课
