2c3f722c81e40a20077bf7b71ff541ba0a510f09
chenyuwangzi <15558177820@163.com>
2020-09-02 10:47:42 +0800

    我的模块

--- src/views/myPage/index.vue
+++ src/views/myPage/index.vue
@@ -5,17 +5,54 @@
         <i class="icon_change"></i>切换学员
       </p>
       <div class="person_wrap">
-        <img src="../../assets/images/home/logo.png" alt />
+        <img src="../../assets/images/my/person.jpg" alt />
         <div class="msg_wrap">
           <p class="name">
             {{msg.name}}
-            <i class='grade'>{{msg.grade}}</i>
+            <i class="grade">{{msg.grade}}</i>
           </p>
           <p class="phone">{{msg.phone}}</p>
         </div>
         <i class="right_arrow"></i>
       </div>
     </div>
+    <ul class="money_wrap">
+      <li>
+        <i class="icon_money"></i>
+        <p>我的钱包</p>
+      </li>
+      <li>
+        <i class="icon_coupon"></i>
+        <p>优惠券</p>
+      </li>
+    </ul>
+    <ul class="sort_wrap">
+      <li>
+        <i class="order"></i>
+        <span>我的订单</span>
+        <i class="enter_arrow"></i>
+      </li>
+       <li>
+        <i class="course"></i>
+        <span>我的课程</span>
+        <i class="enter_arrow"></i>
+      </li>
+       <li>
+        <i class="message"></i>
+        <span>消息中心</span>
+        <i class="enter_arrow"></i>
+      </li>
+       <li>
+        <i class="advice"></i>
+        <span>意见反馈</span>
+        <i class="enter_arrow"></i>
+      </li>
+       <li>
+        <i class="setting"></i>
+        <span>设置</span>
+        <i class="enter_arrow"></i>
+      </li>
+    </ul>
     <div class="tabbar_wrap">
       <van-tabbar v-model="active" active-color="#E94831" inactive-color="#A1A5BB">
         <van-tabbar-item icon="home-o" to="/homePage/index">
@@ -53,10 +90,10 @@ import { Dialog } from "vant";
 export default {
   data() {
     return {
-      msg:{
-        name:'肖剑',
-        grade:'初二',
-        phone:'15967119576'
+      msg: {
+        name: "肖剑",
+        grade: "初二",
+        phone: "15967119576",
       },
       active: 3,
       icon: {
@@ -100,9 +137,10 @@ export default {
   padding-bottom: 50px;
   .wrap_top {
     padding: 25px 25px 0 20px;
-    height: 171px;
-    background: #eb4f37;
-    color:#fff;
+    height: 190px;
+    background: url("../../assets/images/my/bg.png");
+    background-size: cover;
+    color: #fff;
     .change_student {
       text-align: right;
       line-height: 21px;
@@ -116,7 +154,7 @@ export default {
       width: 15px;
       height: 15px;
       display: inline-block;
-      background: url("../../assets/images/home/shopping.svg");
+      background: url("../../assets/images/my/change.svg");
       background-size: cover;
       position: absolute;
       right: 68px;
@@ -132,31 +170,127 @@ export default {
       border: 1px solid #979797;
       margin-right: 16px;
     }
-    .name{
+    .name {
       font-size: 18px;
     }
-    .grade{
+    .grade {
       display: inline-block;
       margin-left: 7px;
       font-style: normal;
       border-radius: 10px;
-      border: 1px solid #FFFFFF;
+      border: 1px solid #ffffff;
       font-size: 12px;
-      padding:2px 10px;
+      padding: 2px 10px;
     }
-    .phone{
+    .phone {
       font-size: 15px;
       line-height: 21px;
     }
-    .right_arrow{
+    .right_arrow {
       display: inline-block;
-      width: 15px;
-      height: 15px;
+      width: 8px;
+      height: 13px;
       display: inline-block;
-      background: url("../../assets/images/home/shopping.svg");
+      background: url("../../assets/images/my/white_more.svg");
+      background-size: cover;
+      position: absolute;
+      right: 25px;
+    }
+  }
+  .money_wrap {
+    margin-top: 14px;
+    padding: 0 20px;
+    display: flex;
+    justify-content: space-between;
+    li {
+      width: 160px;
+      height: 60px;
+      background: #ffffff;
+      box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.05);
+      border-radius: 4px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .icon_money {
+      display: inline-block;
+      background: url("../../assets/images/my/money.svg");
+      background-size: cover;
+      width: 25px;
+      height: 21px;
+    }
+    .icon_coupon {
+      display: inline-block;
+      background: url("../../assets/images/my/coupon.svg");
+      background-size: cover;
+      width: 31px;
+      height: 21px;
+    }
+    p {
+      font-size: 15px;
+      color: #333333;
+      margin-left: 20px;
+    }
+  }
+  .sort_wrap {
+    padding: 12px 0 0 25px;
+    li {
+      display: flex;
+      align-items: center;
+      border-bottom: 1px solid #e2e2e2;
+      height: 58px;
+      color: #666666;
+      font-size: 18px;
+      position: relative;
+    }
+    .order {
+      width: 23px;
+      height: 20px;
+      display: inline-block;
+      background: url("../../assets/images/my/icon_order.svg");
+      background-size: cover;
+      margin-right: 18px;
+    }
+    .enter_arrow{
+      width: 8px;
+      height: 13px;
+      display: inline-block;
+      background: url("../../assets/images/my/more.svg");
       background-size: cover;
       position: absolute;
       right:25px;
+    }
+    .course{
+      width: 21px;
+      height: 23px;
+      display: inline-block;
+      background: url("../../assets/images/my/icon_course.svg");
+      background-size: cover;
+      margin-right: 18px;
+    }
+    .message{
+      width: 23px;
+      height: 23px;
+      display: inline-block;
+      background: url("../../assets/images/my/icon_message.svg");
+      background-size: cover;
+      margin-right: 18px;
+    }
+    .advice{
+      width: 23px;
+      height: 21px;
+      display: inline-block;
+      background: url("../../assets/images/my/icon_advice.svg");
+      background-size: cover;
+      margin-right: 18px;
+    }
+    .setting{
+      width: 23px;
+      height: 23px;
+      display: inline-block;
+      background: url("../../assets/images/my/icon_setting.svg");
+      background-size: cover;
+      margin-right: 18px;
     }
   }
 }
